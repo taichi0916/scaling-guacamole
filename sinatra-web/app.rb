be34cd14602ab3f40d39acf58b6f4360db0980bc
taichi imai <imai0916taiti@gmail.com>
2017-10-23 13:24:31 +0000

    init

---
+++ sinatra-web/app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get'/'do
+  erb :index
+end
+
+post '/'do
+  erb :index
+end
+
+
+
---
+++ test.rb
@@ -0,0 +1,29 @@
+require 'erb'
+
+def web_page
+  <<-PAGE
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+    <meta charset="UTF-8">
+    <title>タイトル</title>
+ </head>
+ <body>
+
+ 
+ </body>
+</html>
+  PAGE
+end
+
+erb = ERB.new(web_page)
+result = erb.result
+puts result
+
+
+
+
+
+
+
+
