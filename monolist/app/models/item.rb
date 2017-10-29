ad8933d60dc24d59198ab50a01a502fabb84d8a7
taichi imai <imai0916taiti@gmail.com>
2017-10-29 12:22:06 +0000

    want

--- monolist/app/models/item.rb
+++ monolist/app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   validates :name, presence: true, lenght: { maximum 255 }
   validates :url, presence: true, lenght: { maximum 255 }
   validates :image_url, presence: true, lenght: { maximum 255 }
+  
+  has_many :ownerships
+  has_many :users, through: :ownerships
+  has_many :wants
+  has_many :want_users, through: :wants, class_name: 'User', source: :user
 end
